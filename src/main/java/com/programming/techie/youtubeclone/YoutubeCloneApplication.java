c123637a10515ed3346f5bebe279f0ee2e8a6393
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2022-01-03 12:46:28 -0300

    multi-modulos

--- src/main/java/com/programming/techie/youtubeclone/YoutubeCloneApplication.java
+++ src/main/java/com/programming/techie/youtubeclone/YoutubeCloneApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YoutubeCloneApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(YoutubeCloneApplication.class, args);
 	}
